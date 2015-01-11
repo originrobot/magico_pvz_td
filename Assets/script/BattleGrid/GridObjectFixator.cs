3842ad6cb05fc4cbda12c58c28172babc7882f5c
yaning <yaning@gmail.com>
2015-01-11 14:51:32 +0800

    added a prefab for artifact

--- Assets/script/BattleGrid/GridObjectFixator.cs
+++ Assets/script/BattleGrid/GridObjectFixator.cs
@@ -8,13 +8,13 @@ public class GridObjectFixator : MonoBehaviour
 		// Update is called once per frame
 		void Update ()
 		{
-			if (Application.isPlaying == false) {
-					Vector2 coord = BattleGrid.instance.positionToCoord (transform.position);
-					coord.x = Mathf.Clamp(coord.x, 0, BattleGrid.instance.cols - 5);
-					coord.y = Mathf.Clamp(coord.y, 0, BattleGrid.instance.rows - 1);
-					this.transform.position = BattleGrid.instance.coordToPosition (coord);
-//					Debug.Log("dafasdf"+transform.position.z);
-				}
+//			if (Application.isPlaying == false) {
+//					Vector2 coord = BattleGrid.instance.positionToCoord (transform.position);
+//					coord.x = Mathf.Clamp(coord.x, 0, BattleGrid.instance.cols - 5);
+//					coord.y = Mathf.Clamp(coord.y, 0, BattleGrid.instance.rows - 1);
+//					this.transform.position = BattleGrid.instance.coordToPosition (coord);
+////					Debug.Log("dafasdf"+transform.position.z);
+//				}
 		}
 }
 
