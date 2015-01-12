36014898be03f16ef9120bb4da9ae570dc8e4f47
Raven <raven@poopies.cn>
2015-01-12 15:27:25 +0800

    initial commit of warlords' classes

---
+++ Assets/script/BattleCore/Warlord.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Warlord : MonoBehaviour 
+{
+	// hp
+	public int maxHP;
+	private int currentHP;
+
+	// level
+	private int currentLevel = 1;
+	public List<int> levelUpCost;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
