9ef0b8d883ad551f2b381cf9c547043da57abdac
Raven <raven@poopies.cn>
2015-01-13 10:07:29 +0800

    member variables for CD boost artifact

--- Assets/script/BattleGrid/CDBoostUnit.cs
+++ Assets/script/BattleGrid/CDBoostUnit.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CDBoostUnit : Unit
 {
+	public List<float> cooldownTimes;
 
 	// Use this for initialization
 	void Start () {
